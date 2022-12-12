eac6c07b0d14e919b0b6344287ba597a68790105
Wilko Henecka <3759021+wilko77@users.noreply.github.com>
2022-12-12 16:07:43 +1100

    add ec2:DescribeInstanceTypes permission

--- iam.tf
+++ iam.tf
@@ -10,7 +10,8 @@ data "aws_iam_policy_document" "kubernetes_cluster_autoscaler" {
       "autoscaling:DescribeTags",
       "autoscaling:SetDesiredCapacity",
       "autoscaling:TerminateInstanceInAutoScalingGroup",
-      "ec2:DescribeLaunchTemplateVersions"
+      "ec2:DescribeLaunchTemplateVersions",
+      "ec2:DescribeInstanceTypes"
     ]
     resources = [
       "*",
